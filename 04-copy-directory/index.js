203e2a19b0b1b7c342ba9e2864e63021dcfc51da
Ilya Hancharuk <innot707@gmail.com>
2022-11-04 21:42:51 +0300

    feat: 04-copy-directory task complited

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs')
+const path = require('path')
 
+const dir = path.join(__dirname, './files')
+const dirCopy = path.join(__dirname, './files-copy')
+
+function copyDir (dir, dirCopy) {
+  fs.mkdir(dirCopy, { recursive: true }, error => {
+    if (error) return console.error(error.message)
+  })
+  fs.readdir(dir, {withFileTypes: true}, (error, files) => {
+    if (error) return console.error(error.message)
+    files.forEach((file) => {
+      let copiedFile = dir + '\\' + file.name.toString()
+      let newFile = dirCopy + '\\' + file.name.toString()
+      fs.copyFile(copiedFile, newFile, error => {
+        if (error) return console.error(error.message)
+      })
+    })
+    console.log('Копирование завершено');
+  })
+}
+
+copyDir(dir, dirCopy)
