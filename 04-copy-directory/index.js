05169ddecbd1ac5bb1fd426f271798a398b7a910
Ilya Hancharuk <innot707@gmail.com>
2022-11-07 17:32:32 +0300

    feat: add updating folder and copying subfolders in 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,21 +4,40 @@ const path = require('path')
 const dir = path.join(__dirname, './files')
 const dirCopy = path.join(__dirname, './files-copy')
 
-function copyDir (dir, dirCopy) {
+fs.access(dirCopy, error => {
+  if(!error) {
+    fs.rm(dirCopy, { recursive: true }, error => {
+      if (error) return console.error(error.message)
+      copyDir(dir, dirCopy)
+    })
+  } else {
+    copyDir(dir, dirCopy)
+  }
+  console.log('Копирование завершено')
+})
+
+function copyDir(dir, dirCopy) {
   fs.mkdir(dirCopy, { recursive: true }, error => {
     if (error) return console.error(error.message)
   })
+  
   fs.readdir(dir, {withFileTypes: true}, (error, files) => {
     if (error) return console.error(error.message)
     files.forEach((file) => {
-      let copiedFile = dir + '\\' + file.name.toString()
-      let newFile = dirCopy + '\\' + file.name.toString()
-      fs.copyFile(copiedFile, newFile, error => {
-        if (error) return console.error(error.message)
-      })
+      if ((!file.isDirectory())) {
+        let copiedFile = dir + '\\' + file.name.toString()
+        let newFile = dirCopy + '\\' + file.name.toString()
+        fs.copyFile(copiedFile, newFile, error => {
+          if (error) return console.error(error.message)
+        })
+      } else {
+        let deepDir = path.join(dir, file.name.toString())
+        let deepDirCopy = path.join(dirCopy, file.name.toString())
+        fs.mkdir(deepDirCopy, { recursive: true }, error => {
+          if (error) return console.error(error.message)
+        })
+        copyDir(deepDir, deepDirCopy)
+      }
     })
-    console.log('Копирование завершено');
   })
 }
-
-copyDir(dir, dirCopy)
